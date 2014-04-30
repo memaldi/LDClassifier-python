074774fd7e6433852720db0b3ded6a23f5b7a95e
Mikel Emaldi <m.emaldi@gmail.com>
2014-04-30 12:53:14 +0200

    Matching classes

--- match-graphs.py
+++ match-graphs.py
@@ -5,14 +5,18 @@ import uuid
 import sys
 import redis
 import requests
+import stringdistances
 from rdflib import Graph
 from os import listdir
 from os.path import isfile, join
 
 ACCEPT_LIST = ['application/rdf+xml', 'text/n3', 'text/plain', 'application/owl+xml']
 RDFLIB_CORRESPONDENCE = {'application/rdf+xml': 'xml', 'text/n3': 'n3', 'text/plain': 'nt', 'application/owl+xml': 'xml'}
+BLACKLIST = []
+MATCHING_FUNCTIONS = ['substring_distance', 'equal_distance', 'levenshtein_distance', 'smoa_distance']
 
 def get_namespace(url):
+    url = url.replace('<', '').replace('>', '').replace('"', '')
     if '#' in url:
         return url.split('#')[0]
     else:
@@ -23,7 +27,6 @@ def get_namespace(url):
         return new_url
 
 def get_ontology(r, namespace, args):
-    namespace = namespace.replace('<', '').replace('>', '').replace('"', '')
     ontology_file = r.get('%s:%s:file' % (args.prefix, namespace))
     ontology_serialization = None
     if ontology_file != None:
@@ -39,9 +42,10 @@ def get_ontology(r, namespace, args):
                     found = True
                     break
             except Exception as e:
+                BLACKLIST.append(namespace)
                 break
 
-        if not found:
+        if not found and namespace not in BLACKLIST:
             try:
                 request = requests.get(namespace, timeout=10)
                 for accept in ACCEPT_LIST:
@@ -78,6 +82,44 @@ def parse_ontology(ontology_file, serialization):
                 pass
     return None
 
+def get_classes(ontology):
+    class_list = []
+    sparql_result = ontology.query('SELECT DISTINCT ?s WHERE {?s a owl:Class}')
+    if len(sparql_result) == 0:
+        sparql_result = ontology.query('SELECT DISTINCT ?s WHERE {?s a rdfs:Class}')
+    for s in sparql_result:
+        class_list.append(s)
+    return class_list
+
+def get_entity_name(url):
+    if '#' in url:
+        return url[1]
+    else:
+        if url[len(url) - 1] == '/':
+            url = url[:len(url) - 1]
+        surl = url.split('/')
+        return surl[len(surl) - 1]
+
+def distance(source, target, function):
+    source_name = get_entity_name(source[0])
+    target_name = get_entity_name(target[0])
+    method = getattr(stringdistances, function)
+    method(source_name, target_name)
+
+def match_classes(source_ontology, target_ontology):
+    source_classes = get_classes(source_ontology)
+    target_classes = get_classes(target_ontology)
+    total = len(source_classes)
+    count = 1
+    for source_class in source_classes:
+        sys.stdout.write("\rMatching classes (%s/%s)..." % (count, total))
+        sys.stdout.flush()
+        count += 1
+        for target_class in target_classes:
+            for function in MATCHING_FUNCTIONS:
+                dist = distance(source_class, target_class, function)
+                print source_class, target_class, function, dist
+    print ''
 
 def generate_alignment(args):
     alignment_connection = happybase.Connection(args.hbase_host, port=args.hbase_port)
@@ -103,14 +145,25 @@ def generate_alignment(args):
             else:
                 label = data['edge:label']
             namespace = get_namespace(label)
-            if not r.exists('%s:%s:file' % (args.prefix, namespace)):
+            if not r.exists('%s:%s:file' % (args.prefix, namespace)) and namespace not in BLACKLIST:
                 get_ontology(r, namespace, args)
     print ''
 
     # Match ontologies
-    for source_key in r.keys('%s:*' % args.prefix):
-        for target_key in r.keys('%s:*' % args.prefix):
-            pass
+    total = len(r.keys('%s:*:file' % args.prefix))
+    count = 1
+    for source_key in r.keys('%s:*:file' % args.prefix):
+        sys.stdout.write("\rMatching ontologies (%s/%s)..." % (count, total))
+        sys.stdout.flush()
+        count += 1
+        for target_key in r.keys('%s:*:file' % args.prefix):
+            if source_key != target_key:
+                source_ontology = parse_ontology(r.get(source_key), r.get(source_key.replace(':file', ':serialization')))
+                if source_ontology != None:
+                    target_ontology = parse_ontology(r.get(source_key), r.get(source_key.replace(':file', ':serialization')))
+                    match_classes(source_ontology, target_ontology)
+    print ''
+
     connection.close()
 
 def reset(args):
@@ -126,7 +179,6 @@ def reset(args):
         count += 1
     connection.close()
     print ''
-
     print 'Done!'
 
 def create_table(table_name, connection, rewrite=False):
@@ -176,7 +228,7 @@ subparsers = parser.add_subparsers(help='action to perform', dest='command')
 
 parser_load = subparsers.add_parser('load', help='load subgraphs into database')
 parser_load.add_argument('graph_dir', help='directory containing substructures extracted by SUBDUE')
-parser_load.add_argument('rewrite', help='if a graph already exists, is replaced in database', default=False, type=bool)
+parser_load.add_argument('-rewrite', help='if a graph already exists, is replaced in database. Default: False', default=False, type=bool)
 
 parser_reset = subparsers.add_parser('reset', help='drops all tables from the database')
 
---
+++ stringdistances.py
@@ -0,0 +1,191 @@
+def substring_distance(string1, string2):
+    if string1 == None or string2 == None:
+        return 1.0
+    
+    len1 = len(string1)
+    len2 = len(string2)
+    
+    if len1 == 0 and len2 == 0:
+        return 0.0
+    if len1 == 0 or len2 == 0:
+        return 1.0
+    best = 0
+    i = 0
+    while i < len1 and len1 - 1 > best:
+        j = 0
+        while len2 - j > best:
+            k = i;   
+            while j < len2 and string1[k] != string2[j]:
+                j += 1
+            if j != len2:
+                j += 1
+                k += 1
+                while j < len2 and k < len1 and string1[k] == string2[j]:  
+                    j += 1 
+                    k += 1
+                best = max(best, k-i)
+        i += 1
+    return 1.0 - (float(2 * best) / float(len1 + len2))
+    
+
+def equal_distance(string1, string2):
+    if string1 == None or string2 == None:
+        return 1.0
+    if string1 == string2:
+        return 0.0
+    else:
+        return 1.0
+        
+def needlemanwunsch_distance(string1, string2, gap):
+    if string1 == None or string2 == None:
+        return 1.0
+    
+    n = len(string1)
+    m = len(string2)
+    
+    if n == 0 or m == 0:
+        return 1.0
+    
+    p = []
+    d = []
+    
+    for i in xrange(n+1):
+        p.append(i * gap)
+    for j in xrange(1, m+1):
+        t_j = string2[j-1]
+        d.append(j * gap)
+        
+        for i in xrange(1, n+1):
+            if string1[i-1] == t_j:
+                cost = 0
+            else:
+                cost = 1
+            d.append(min(min(d[i-1] + gap, p[i] + gap), p[i-1] + cost))
+        d_temp = p
+        p = d
+        d = d_temp
+        d = []
+    min_value = min(n, m)
+    diff = max(n, m) - min_value
+    return float(p[n]) / float(min_value + diff * gap)
+    
+def levenshtein_distance(string1, string2):
+    return needlemanwunsch_distance(string1, string2, 1)
+    
+def _normalize(string):
+    string = string.replace('.', '')
+    string = string.replace('_', '')
+    string = string.replace(' ', '')
+    return string
+    
+def _winkler_improvement(string1, string2, commonality):
+    n = min(len(string1), len(string2))
+    for i in xrange(n):
+        if string1[i] != string2[i]:
+            break
+    common_prefix_length = min(4, i)
+    winkler = common_prefix_length * 0.1 * (1 - commonality)
+    return winkler
+    
+def _score(string1, string2):
+    if string1 == None or string2 == None:
+        return -1
+    
+    s1 = string1.lower()
+    s2 = string2.lower()
+    s1 = _normalize(s1)
+    s2 = _normalize(s2)
+    
+    l1 = len(s1)
+    l2 = len(s2)
+    
+    L1 = l1
+    L2 = l2
+    
+    if l1 == 0 and l2 == 0:
+        return 1.0
+    if l1 == 0 or l2 == 0:
+        return 0.0
+    
+    common = 0.0
+    best = 2
+    #max_value = min(l1, l2)
+    
+    while len(s1) > 0 and len(s2) > 0 and best != 0:
+        best = 0
+        l1 = len(s1)
+        l2 = len(s2)
+        i = 0
+        
+        start_s1, start_s2, end_s1, end_s2, p = 0, 0, 0, 0, 0
+        
+        while i < l1 and l1 - i > best:
+            j = 0
+            while l2 - j > best:
+                k = i
+                while j < l2 and s1[k] != s2[j]:
+                    j += 1
+                if j != l2:
+                    p = j
+                    j += 1
+                    k += 1
+                    while j < l2 and k < l1 and s1[k] == s2[j]:
+                        j += 1
+                        k += 1
+                    if k - i > best:
+                        best = k - i
+                        start_s1 = i
+                        end_s1 = k;
+                        start_s2 = p;
+                        end_s2 = j;
+            i += 1
+        new_string = ''
+    
+        for i in xrange(len(s1)):
+            if i >= start_s1 and i < end_s1:
+                continue
+            new_string += s1[i]
+        s1 = new_string
+        new_string = ''
+        
+        for i in xrange(len(s2)):
+            if i >= start_s2 and i < end_s2:
+                continue
+            new_string += s2[i]        
+        
+        s2 = new_string
+        
+        if best > 2:
+            common += best
+        else:
+            best = 0
+    
+    commonality = (2 * common) / (L1 + L2)
+
+    wi = _winkler_improvement(string1, string2, commonality)
+    rest1 = L1 - common
+    rest2 = L2 - common
+    
+    #unmatched_s1 = max(rest1, 0)
+    #unmatched_s2 = max(rest2, 0)
+    unmatched_s1 = rest1/L1
+    unmatched_s2 = rest2/L2
+    
+    suma = unmatched_s1 + unmatched_s2
+    product = unmatched_s1 * unmatched_s2
+    
+    p = 0.6
+    
+    if suma - product == 0:
+        dissimilarity = 0
+    else:
+        dissimilarity = float(product) / float(p + (1 - p) * (suma - product))
+        
+    result = commonality - dissimilarity + wi
+    return (result + 1) / 2
+              
+def smoa_distance(string1, string2):
+    if string1 == None or string2 == None:
+        return 1.0
+    return 1.0 - _score(string1, string2)
+    
