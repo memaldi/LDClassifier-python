d7440b2429515b3a3eae67704f1fa83154d25149
Mikel Emaldi <m.emaldi@gmail.com>
2014-05-01 17:23:45 +0200

    Fixed minor bugs

--- match-graphs.py
+++ match-graphs.py
@@ -3,10 +3,7 @@ import happybase
 import struct
 import uuid
 import sys
-import redis
-import requests
 import stringdistances
-from rdflib import Graph
 from os import listdir
 from os.path import isfile, join
 
@@ -26,71 +23,6 @@ def get_namespace(url):
             new_url += chunk + '/'
         return new_url
 
-def get_ontology(r, namespace, args):
-    ontology_file = r.get('%s:%s:file' % (args.prefix, namespace))
-    ontology_serialization = None
-    if ontology_file != None:
-        ontology_serialization = r.get('%s:%s:serialization' % (args.prefix, namespace))
-    else:
-        found = False
-        for accept in ACCEPT_LIST:
-            headers = {'Accept': accept}
-            try:
-                request = requests.get(namespace, headers=headers, timeout=10)
-                if accept in request.headers['content-type']:
-                    ontology_serialization = accept
-                    found = True
-                    break
-            except Exception as e:
-                BLACKLIST.append(namespace)
-                break
-
-        if not found and namespace not in BLACKLIST:
-            try:
-                request = requests.get(namespace, timeout=10)
-                for accept in ACCEPT_LIST:
-                    if accept in request:
-                        ontology_serialization = accept
-            except:
-                pass
-
-        if ontology_serialization in RDFLIB_CORRESPONDENCE:
-            try:
-                ontology_file = '%s/%s' % (args.ontology_dir, str(uuid.uuid4()))
-                ontology_serialization = RDFLIB_CORRESPONDENCE[ontology_serialization]
-                f = open(ontology_file, 'w')
-                f.write(request.text)
-                f.close()
-                r.set('%s:%s:file' % (args.prefix, namespace), ontology_file)
-                r.set('%s:%s:serialization' % (args.prefix, namespace), ontology_serialization)
-            except:
-                pass
-
-    return ontology_file, ontology_serialization
-
-def parse_ontology(ontology_file, serialization):
-    ontology = Graph()
-    try:
-        ontology.parse(ontology_file, format=serialization)
-        return ontology
-    except:
-        for key in RDFLIB_CORRESPONDENCE:
-            try:
-                ontology.parse(ontology_file, format=RDFLIB_CORRESPONDENCE[key])
-                return ontology
-            except:
-                pass
-    return None
-
-def get_classes(ontology):
-    class_list = []
-    sparql_result = ontology.query('SELECT DISTINCT ?s WHERE {?s a owl:Class}')
-    if len(sparql_result) == 0:
-        sparql_result = ontology.query('SELECT DISTINCT ?s WHERE {?s a rdfs:Class}')
-    for s in sparql_result:
-        class_list.append(s)
-    return class_list
-
 def get_entity_name(url):
     if '#' in url:
         return url.split('#')[1]
@@ -106,35 +38,22 @@ def distance(source, target, function):
     method = getattr(stringdistances, function)
     return method(source_name, target_name)
 
-def match_classes(source_ontology, target_ontology):
-    source_classes = get_classes(source_ontology)
-    target_classes = get_classes(target_ontology)
-    total = len(source_classes)
-    count = 1
-    for source_class in source_classes:
-        sys.stdout.write("\rMatching classes (%s/%s)..." % (count, total))
-        sys.stdout.flush()
-        count += 1
-        for target_class in target_classes:
-            for function in MATCHING_FUNCTIONS:
-                dist = distance(source_class, target_class, function)
-                print source_class, target_class, function, dist
-    print ''
-
 def clean_label(label):
     label = label.replace('<', '').replace('>', '').replace('"', '')
     return label
 
 def generate_alignment(args):
     alignment_connection = happybase.Connection(args.hbase_host, port=args.hbase_port)
-    #alignment_connection.create_table('alignments', {'cf': dict()})
+    try:
+        alignment_connection.create_table('alignments', {'cf': dict()})
+    except:
+        pass
     alignment_table = alignment_connection.table('alignments')
 
     connection = happybase.Connection(args.hbase_host, port=args.hbase_port, table_prefix=args.prefix)
     tables = connection.tables()
 
     # Get all Classes and properties
-    r = redis.StrictRedis(host=args.redis_host, port=args.redis_port, db=args.redis_db)
     total = len(tables)
     count = 1
     for table_name in tables:
@@ -147,16 +66,21 @@ def generate_alignment(args):
             if 'vertex:label' in data:
                 label = data['vertex:label']
                 # Store Class
-                alignment_table.put(label, {'cf:type': 'Class'})
+                row = table.row(label)
+                if row:
+                    alignment_table.put(label, {'cf:type': 'Class'})
             else:
                 label = data['edge:label']
                 # Store property
-                alignment_table.put(label, {'cf:type': 'property'})
+                row = table.row(label)
+                if row:
+                    alignment_table.put(label, {'cf:type': 'property'})
 
     print ''
 
     # Match Classes and properties
     # Match Classes
+    print 'Matching classes...'
     for source_key, source_data in alignment_table.scan(filter="SingleColumnValueFilter ('cf', 'type', =, 'binary:Class', true, false)"):
         for target_key, target_data in alignment_table.scan(filter="SingleColumnValueFilter ('cf', 'type', =, 'binary:Class, true, false')"):
             if source_key != target_key and get_namespace(source_key) != get_namespace(target_key):
@@ -170,6 +94,7 @@ def generate_alignment(args):
                         alignment_table.put(str(uuid.uuid4()), {'cf:source': source_key, 'cf:target': target_key, 'cf:function': function, 'cf:dist': struct.pack(">q", dist)})
                         print source_key, target_key, function, dist
     # Match properties
+    print 'Matching properties...'
     for source_key, source_data in alignment_table.scan(filter="SingleColumnValueFilter ('cf', 'type', =, 'binary:property', true, false)"):
         for target_key, target_data in alignment_table.scan(filter="SingleColumnValueFilter ('cf', 'type', =, 'binary:property', true, false)"):
             if source_key != target_key and get_namespace(source_key) != get_namespace(target_key):
@@ -251,7 +176,6 @@ parser_load.add_argument('-rewrite', help='if a graph already exists, is replace
 parser_reset = subparsers.add_parser('reset', help='drops all tables from the database')
 
 parser_generate_alignment = subparsers.add_parser('generate_alignment', help='generate alignments among classes and properties found in substructures')
-#parser_generate_alignment.add_argument('-ontology_dir', help='Directory where store ontologies', default='/tmp')
 
 args = parser.parse_args()
 
