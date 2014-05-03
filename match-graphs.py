8a23102f6979b3e52053a44d2a02647b71fb51f1
Mikel Emaldi <m.emaldi@gmail.com>
2014-05-03 22:27:28 +0200

    Implemented basic_sinonym_distance and test

--- match-graphs.py
+++ match-graphs.py
@@ -12,7 +12,7 @@ from os.path import isfile, join
 ACCEPT_LIST = ['application/rdf+xml', 'text/n3', 'text/plain', 'application/owl+xml']
 RDFLIB_CORRESPONDENCE = {'application/rdf+xml': 'xml', 'text/n3': 'n3', 'text/plain': 'nt', 'application/owl+xml': 'xml'}
 BLACKLIST = []
-MATCHING_FUNCTIONS = ['substring_distance', 'equal_distance', 'levenshtein_distance', 'smoa_distance']
+MATCHING_FUNCTIONS = ['substring_distance', 'equal_distance', 'levenshtein_distance', 'smoa_distance', 'basic_sinonym_distance']
 
 def get_namespace(url):
     url = url.replace('<', '').replace('>', '').replace('"', '')
@@ -82,7 +82,7 @@ def generate_alignment(args):
 
     # Match Classes and properties
     # Match Classes
-    print 'Matching classes...'
+    print 'Matching classes, please wait...'
     for source_key, source_data in alignment_table.scan(filter="SingleColumnValueFilter ('cf', 'type', =, 'binary:Class', true, true)"):
         for target_key, target_data in alignment_table.scan(filter="SingleColumnValueFilter ('cf', 'type', =, 'binary:Class', true, true)"):
             if source_key != target_key and get_namespace(source_key) != get_namespace(target_key):
@@ -96,7 +96,7 @@ def generate_alignment(args):
                         alignment_table.put(str(uuid.uuid4()), {'cf:source': source_key, 'cf:target': target_key, 'cf:function': function, 'cf:dist': struct.pack(">f", dist)})
                         #print source_key, target_key, function, dist
     # Match properties
-    print 'Matching properties...'
+    print 'Matching properties, please wait...'
     for source_key, source_data in alignment_table.scan(filter="SingleColumnValueFilter ('cf', 'type', =, 'binary:property', true, true)"):
         for target_key, target_data in alignment_table.scan(filter="SingleColumnValueFilter ('cf', 'type', =, 'binary:property', true, true)"):
             if source_key != target_key and get_namespace(source_key) != get_namespace(target_key):
@@ -113,6 +113,10 @@ def generate_alignment(args):
 
 def reset(args):
     connection = happybase.Connection(args.hbase_host, port=args.hbase_port, table_prefix=args.prefix)
+    alignment_connection = happybase.Connection(args.hbase_host, port=args.hbase_port)
+    alignment_connection.disable_table('alignments')
+    alignment_connection.delete_table('alignments')
+    alignment_connection.close()
     tables = connection.tables()
     total = len(tables)
     count = 1
@@ -265,6 +269,81 @@ def match_vertexes(source_table_name, target_table_name, connection, alignment_c
     return vertex_replace_dict
 
 def sim(args):
+    if args.no_matching != None:
+        return no_matching(args)
+    else:
+        return sim_matching(args)
+
+def no_matching(args):
+
+    result_dict = {}
+
+    connection = happybase.Connection(args.hbase_host, port=args.hbase_port, table_prefix=args.prefix)
+    alignment_connection = happybase.Connection(args.hbase_host, port=args.hbase_port)
+    alignment_table = alignment_connection.table('alignments')
+    tables = connection.tables()
+
+    total = len(tables)
+    count = 1
+    for source_table_name in tables:
+        sys.stdout.write("\rMatching subgraphs (%s/%s)..." % (count, total))
+        sys.stdout.flush()
+        count += 1
+        source_table = connection.table(source_table_name)
+        for target_table_name in tables:
+            if source_table_name != target_table_name:
+                target_table = connection.table(target_table_name)
+                source_file_name = '%s/%s' % (args.tmp_dir, str(uuid.uuid4()))
+                source_file = open(source_file_name, 'w')
+                target_file_name = '%s/%s' % (args.tmp_dir, str(uuid.uuid4()))
+                target_file = open(target_file_name, 'w')
+                source_len = 0
+                target_len = 0
+
+                # Write vertexes in source graph
+                for key, data in source_table.scan(filter="SingleColumnValueFilter ('graph', 'type', =, 'binary:v', true, false)"):
+                    label = data['vertex:label']
+                    source_file.write('v %s %s\n' % (struct.unpack(">q", key)[0], label))
+                    source_len += 1
+                # Write vertexes in target graph
+                for key, data in target_table.scan(filter="SingleColumnValueFilter ('graph', 'type', =, 'binary:v', true, false)"):
+                    label = data['vertex:label']
+                    target_file.write('v %s %s\n' % (struct.unpack(">q", key)[0], label))
+                    target_len += 1
+                # Write edges in source graph
+                for key, data in source_table.scan(filter="SingleColumnValueFilter ('graph', 'type', =, 'binary:e', true, false)"):
+                    label = data['edge:label']
+                    source_file.write('d %s %s %s\n' % (struct.unpack(">q", data['edge:source'])[0], struct.unpack(">q", data['edge:target'])[0], label))
+                    source_len += 1
+                # Write edges in target graph
+                for key, data in target_table.scan(filter="SingleColumnValueFilter ('graph', 'type', =, 'binary:e', true, false)"):
+                    label = data['edge:label']
+                    target_file.write('d %s %s %s\n' % (struct.unpack(">q", data['edge:source'])[0], struct.unpack(">q", data['edge:target'])[0], label))
+                    target_len += 1
+                source_file.close()
+                target_file.close()
+
+                # Match graph using GM tool
+                proc = subprocess.Popen([args.subdue_dir + "/bin/gm", source_file_name, target_file_name], stdout=subprocess.PIPE)
+                stdout = proc.stdout.read()
+                transformation_cost = int(stdout[stdout.find('Match Cost = ') + len('Match Cost = '):stdout.find('\n')].split('.')[0])
+                if source_len > target_len:
+                    max_len = source_len
+                else:
+                    max_len = target_len
+                # To be fixed
+                if transformation_cost > max_len:
+                    transformation_cost = max_len
+                normalized_cost = (float(transformation_cost) / max_len)
+                similarity = 1 - normalized_cost
+
+                if source_table_name not in result_dict:
+                    result_dict[source_table_name] = {}
+                result_dict[source_table_name][target_table_name] = similarity
+    print ''
+    return result_dict
+
+def sim_matching(args):
 
     result_dict = {}
 
@@ -350,9 +429,18 @@ def test(args):
     with open(args.validation_file) as f:
         for line in f:
             test_list.append(line.replace('\n', ''))
+    if args.o != None:
+        f = open(args.o, 'w')
+        f.write('%s,%s,%s,%s\n' % ('Subs threshold', 'Matching threshold', 'Precision', 'Recall'))
+
+    if args.no_matching != None:
+        matching_threshold_limit = 0.1
+    else:
+        matching_threshold_limit = 1.1
 
     for subs_threshold in np.arange(0, 1.1, 0.1):
-        for matching_threshold in np.arange(0, 1.1, 0.1):
+        for matching_threshold in np.arange(0, matching_threshold_limit, 0.1):
+            result_dict = {}
             print '*' * 10
             print 'Configuration:'
             print 'Substructure similarity threshold: %s' % subs_threshold
@@ -363,22 +451,29 @@ def test(args):
             for key in result_dict:
                 for key2 in result_dict[key]:
                     similarity = result_dict[key][key2]
-                    if similarity > subs_threshold:
-                        if key in test_list and key2 in test_list:
+                    if similarity >= subs_threshold:
+                        if (key in test_list and key2 in test_list):
                             tp += 1
-                        else:
+                        elif (key in test_list and key2 not in test_list) or (key not in test_list and key2 in test_list):
                             fp += 1
                     else:
                         if key in test_list and key2 in test_list:
                             fn += 1
                         else:
                             tn += 1
+            print 'True positives: %s' % tp
+            print 'False positives: %s' % fp
+            print 'True negatives: %s' % tn
+            print 'False negatives: %s' % fn
             precision = float(tp) / (tp + fp)
             print 'Precision: %s' % str(precision)
             recall = float(tp) / (tp + fn)
             print 'Recall: %s' % str(recall)
 
-    sim(args)
+            if args.o != None:
+                f.write('%s,%s,%s,%s\n' % (str(subs_threshold), str(matching_threshold), str(precision), str(recall)))
+    if args.o != None:
+        f.close()
 
 parser = argparse.ArgumentParser(description='Match substructures.')
 parser.add_argument('-prefix', help='prefix for tables. Default: graph', default='graph')
@@ -401,12 +496,15 @@ parser_generate_alignment = subparsers.add_parser('generate_alignment', help='ge
 parser_similarities = subparsers.add_parser('sim', help='generate similarities among datasets')
 parser_similarities.add_argument('matching_threshold', help='value over is considered that two entities represent the same concept', type=float)
 parser_similarities.add_argument('subdue_dir', help='location of SUBDUE')
+parser_similarities.add_argument('--no_matching', help='find simmilarities without applying ontology matching')
 parser_similarities.add_argument('-tmp_dir', help='dir in which SUBDUE input files are stored. Default: /tmp', default='/tmp')
 
 parser_similarities = subparsers.add_parser('test', help='generate evaluation test')
 parser_similarities.add_argument('subdue_dir', help='location of SUBDUE')
 parser_similarities.add_argument('validation_file', help='test file.')
+parser_similarities.add_argument('--no_matching', help='find simmilarities without applying ontology matching')
 parser_similarities.add_argument('-tmp_dir', help='dir in which SUBDUE input files are stored. Default: /tmp', default='/tmp')
+parser_similarities.add_argument('-o', help='output file to store results')
 
 
 args = parser.parse_args()
--- stringdistances.py
+++ stringdistances.py
@@ -1,10 +1,13 @@
+from nltk.corpus import wordnet as wn
+from nltk.tokenize import wordpunct_tokenize
+
 def substring_distance(string1, string2):
     if string1 == None or string2 == None:
         return 1.0
-    
+
     len1 = len(string1)
     len2 = len(string2)
-    
+
     if len1 == 0 and len2 == 0:
         return 0.0
     if len1 == 0 or len2 == 0:
@@ -14,19 +17,19 @@ def substring_distance(string1, string2):
     while i < len1 and len1 - 1 > best:
         j = 0
         while len2 - j > best:
-            k = i;   
+            k = i;
             while j < len2 and string1[k] != string2[j]:
                 j += 1
             if j != len2:
                 j += 1
                 k += 1
-                while j < len2 and k < len1 and string1[k] == string2[j]:  
-                    j += 1 
+                while j < len2 and k < len1 and string1[k] == string2[j]:
+                    j += 1
                     k += 1
                 best = max(best, k-i)
         i += 1
     return 1.0 - (float(2 * best) / float(len1 + len2))
-    
+
 
 def equal_distance(string1, string2):
     if string1 == None or string2 == None:
@@ -35,26 +38,26 @@ def equal_distance(string1, string2):
         return 0.0
     else:
         return 1.0
-        
+
 def needlemanwunsch_distance(string1, string2, gap):
     if string1 == None or string2 == None:
         return 1.0
-    
+
     n = len(string1)
     m = len(string2)
-    
+
     if n == 0 or m == 0:
         return 1.0
-    
+
     p = []
     d = []
-    
+
     for i in xrange(n+1):
         p.append(i * gap)
     for j in xrange(1, m+1):
         t_j = string2[j-1]
         d.append(j * gap)
-        
+
         for i in xrange(1, n+1):
             if string1[i-1] == t_j:
                 cost = 0
@@ -68,16 +71,16 @@ def needlemanwunsch_distance(string1, string2, gap):
     min_value = min(n, m)
     diff = max(n, m) - min_value
     return float(p[n]) / float(min_value + diff * gap)
-    
+
 def levenshtein_distance(string1, string2):
     return needlemanwunsch_distance(string1, string2, 1)
-    
+
 def _normalize(string):
     string = string.replace('.', '')
     string = string.replace('_', '')
     string = string.replace(' ', '')
     return string
-    
+
 def _winkler_improvement(string1, string2, commonality):
     n = min(len(string1), len(string2))
     for i in xrange(n):
@@ -86,39 +89,39 @@ def _winkler_improvement(string1, string2, commonality):
     common_prefix_length = min(4, i)
     winkler = common_prefix_length * 0.1 * (1 - commonality)
     return winkler
-    
+
 def _score(string1, string2):
     if string1 == None or string2 == None:
         return -1
-    
+
     s1 = string1.lower()
     s2 = string2.lower()
     s1 = _normalize(s1)
     s2 = _normalize(s2)
-    
+
     l1 = len(s1)
     l2 = len(s2)
-    
+
     L1 = l1
     L2 = l2
-    
+
     if l1 == 0 and l2 == 0:
         return 1.0
     if l1 == 0 or l2 == 0:
         return 0.0
-    
+
     common = 0.0
     best = 2
     #max_value = min(l1, l2)
-    
+
     while len(s1) > 0 and len(s2) > 0 and best != 0:
         best = 0
         l1 = len(s1)
         l2 = len(s2)
         i = 0
-        
+
         start_s1, start_s2, end_s1, end_s2, p = 0, 0, 0, 0, 0
-        
+
         while i < l1 and l1 - i > best:
             j = 0
             while l2 - j > best:
@@ -140,52 +143,71 @@ def _score(string1, string2):
                         end_s2 = j;
             i += 1
         new_string = ''
-    
+
         for i in xrange(len(s1)):
             if i >= start_s1 and i < end_s1:
                 continue
             new_string += s1[i]
         s1 = new_string
         new_string = ''
-        
+
         for i in xrange(len(s2)):
             if i >= start_s2 and i < end_s2:
                 continue
-            new_string += s2[i]        
-        
+            new_string += s2[i]
+
         s2 = new_string
-        
+
         if best > 2:
             common += best
         else:
             best = 0
-    
+
     commonality = (2 * common) / (L1 + L2)
 
     wi = _winkler_improvement(string1, string2, commonality)
     rest1 = L1 - common
     rest2 = L2 - common
-    
+
     #unmatched_s1 = max(rest1, 0)
     #unmatched_s2 = max(rest2, 0)
     unmatched_s1 = rest1/L1
     unmatched_s2 = rest2/L2
-    
+
     suma = unmatched_s1 + unmatched_s2
     product = unmatched_s1 * unmatched_s2
-    
+
     p = 0.6
-    
+
     if suma - product == 0:
         dissimilarity = 0
     else:
         dissimilarity = float(product) / float(p + (1 - p) * (suma - product))
-        
+
     result = commonality - dissimilarity + wi
     return (result + 1) / 2
-              
+
 def smoa_distance(string1, string2):
     if string1 == None or string2 == None:
         return 1.0
     return 1.0 - _score(string1, string2)
-    
+
+def basic_sinonym_distance(string1, string2):
+    string1 = string1.lower()
+    string2 = string2.lower()
+
+    dist_subs = stringdistances.substring_distance(string1, string2)
+    synsets = wn.synsets(string1, wn.NOUN)
+    if len(synsets) == 0:
+        tokens = wordpunct_tokenize(string1)
+        for token in tokens:
+            synsets_new = wn.synsets(string1, wn.NOUN)
+            if len(synsets_new) > len(synsets):
+                synsets = synsets_new
+    if len(synsets) > 0:
+        for synset in synsets:
+            for lemma in synset.lemmas:
+                dist = stringdistances.substring_distance(lemma.name, string2)
+                if (dist < dist_subs):
+                    dist_subs = dist
+    return dist_subs
